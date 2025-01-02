752eaa9bcbe7e990edba1b7e17a57f90dcb6d8ee
ninguemzin <alm1427@hotmail.com>
2025-01-02 17:00:07 -0300

    exercicio de calcular gasto de combustivel

---
+++ CalcularViagem.js
@@ -0,0 +1,18 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis, sendo elas:
+
+1 - Preço do combustivel;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distancia em KM da viagem;
+
+*/
+
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+let calcularViagem = (distanciaEmKm / kmPorLitros) * precoCombustivel;
+
+console.log(calcularViagem);
